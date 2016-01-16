62efd4755ccf8d33a139868644e5435c1a22cfde
Jonathan RAMIER <jramier@outlook.com>
2016-01-16 22:59:56 +0100

    * add sidebar component to application

---
+++ src/app/sidebar/sidebar.ts
@@ -0,0 +1,18 @@
+/**
+ * personal-site
+ *
+ * @file sidebar.ts
+ * @author Jonathan RAMIER <jramier@live.fr>
+ * @licence NONE
+ * @version 1.0
+ */
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'sidebar',
+    //template: require()
+})
+export class SideBarComponent {
+
+}
+
